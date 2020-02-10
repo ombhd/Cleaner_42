919bf6dd7e3c1a35590f5e70eb66de68dacd9e42
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-02-10 18:38:11 +0100

    get a bug fixed
    When the storage is 0B it displays 0BB lol
    Now it displays 0B

--- Cleaner.sh
+++ Cleaner.sh
@@ -2,7 +2,10 @@
 #Author OMB 
 
 Storage=$(df -h . | grep "$HOME" | awk '{print($4)}' | tr 'i' 'B')
-
+if [ "$Storage" == "0BB" ];
+then
+	Storage="0B"
+fi
 echo -e "\033[33m\n -- Available Storage Before Cleaning : || $Storage || --\033[0m"
 
 echo -e "\033[31m\n -- Cleaning ...\n\033[0m "
@@ -15,6 +18,9 @@ rm -rf ~/.zcompdump*
 find ~/Desktop -name .DS_Store -depth -exec rm {} \;
 
 Storage=$(df -h . | grep "$HOME" | awk '{print($4)}' | tr 'i' 'B')
-
+if [ "$Storage" == "0BB" ];
+then
+	Storage="0B"
+fi
 echo -e "\033[32m -- Available Storage After Cleaning : || $Storage || --\n\033[0m"
 
