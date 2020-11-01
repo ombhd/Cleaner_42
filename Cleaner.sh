3891be2fd365a84f11f86fe1cb8574954e21357e
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-11-01 09:21:39 +0100

    remove Slack, VSCode, Discord caches

--- Cleaner.sh
+++ Cleaner.sh
@@ -24,6 +24,10 @@ rm -rf ~/.cocoapods.42_cache_bak*
 rm -rf ~/.Trash/*
 rm -rf ~/Library/Caches
 rm -rf ~/.zcompdump*
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/*
+rm -rf ~/Library/Application\ Support/discord/Cache/*
+rm -rf ~/Library/Application\ Support/discord/Code\ Cache/js*
 find ~/Desktop -name .DS_Store -depth -exec rm {} \;
 
 Storage=$(df -h . | grep "$HOME" | awk '{print($4)}' | tr 'i' 'B')
