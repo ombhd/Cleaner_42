30da80b254d7bb4c8032b9df96da24a32c56e203
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-11-18 11:11:00 +0100

    Update CleanerInstaller.sh

--- CleanerInstaller.sh
+++ CleanerInstaller.sh
@@ -4,11 +4,11 @@
 
 while true; do
     echo -e "cclean program cleans: \n1 - The Trash. \n2 - 42 cache files from Library directory and from the home directory. \n3 - Some Slack, VSCode and Discord Caches.\n"
-    read -p "[\033[33mDo you really want to install this program ? \033[0m\033[36m" yn
+    read -p $"[\033[33mDo you really want to install this program ? \033[0m\033[36m" yn
     case $yn in
-        [Yy]* ) make install; break;;
+        [Yy]* ) break;;
         [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo -e "\nPlease answer yes or no !";;
     esac
 done
 
