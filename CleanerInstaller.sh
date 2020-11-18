bceefb09d8369133196e676d9ecd22659be8f2c3
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-11-18 10:55:02 +0100

    Update CleanerInstaller.sh

--- CleanerInstaller.sh
+++ CleanerInstaller.sh
@@ -2,6 +2,16 @@
 #Author Omar BOUYKOURNE
 #42login obouykou
 
+while true; do
+    echo -e "cclean is cleaning the Trash and 42 cache files from Library directory and from the home directory, plus some of Chrome, VSCode and Discord Caches."
+    read -p "Do you really want to install this program ? " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 shell_f=$(echo -n $(ls -a ~ | grep ".zshrc"))
 
 if [ "$shell_f" != ".zshrc" ];
