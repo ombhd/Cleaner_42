962124ead7581f307f42765d018ee98042de8c9b
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-12-14 10:47:08 +0100

    Update Cleaner_42.sh

--- Cleaner_42.sh
+++ Cleaner_42.sh
@@ -11,8 +11,8 @@ then
 		rm -rf ./Cleaner_42
 		exit 0
 	fi
-	cp -f ./Cleaner_42/Cleaner_42.sh ~
-	rm -rf ./Cleaner_42
+	cp -f ./Cleaner_42/Cleaner_42.sh ~ &>/dev/null
+	rm -rf ./Cleaner_42 &>/dev/null
 	echo -e "\033[33m\n -- cclean has been updated successfully --\n\033[0m"
 	exit 0
 fi
@@ -26,29 +26,29 @@ echo -e "\033[33m\n -- Available Storage Before Cleaning : || $Storage || --\033
 echo -e "\033[31m\n -- Cleaning ...\n\033[0m "
 
 #42 Caches
-rm -rf ~/Library/*.42*
-rm -rf ~/*.42*
-rm -rf ~/.zcompdump*
-rm -rf ~/.cocoapods.42_cache_bak*
+rm -rf ~/Library/*.42* &>/dev/null
+rm -rf ~/*.42* &>/dev/null
+rm -rf ~/.zcompdump* &>/dev/null
+rm -rf ~/.cocoapods.42_cache_bak* &>/dev/null
 
 #Trash
-rm -rf ~/.Trash/*
+rm -rf ~/.Trash/* &>/dev/null
 
 #General Cache files
-rm -rf ~/Library/Caches
+rm -rf ~/Library/Caches &>/dev/null
 
 #Slack, VSCode, Discord Caches
-rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
-rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/*
-rm -rf ~/Library/Application\ Support/discord/Cache/*
-rm -rf ~/Library/Application\ Support/discord/Code\ Cache/js*
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* &>/dev/null
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/* &>/dev/null
+rm -rf ~/Library/Application\ Support/discord/Cache/* &>/dev/null
+rm -rf ~/Library/Application\ Support/discord/Code\ Cache/js* &>/dev/null
 
 #.DS_Store files
-find ~/Desktop -name .DS_Store -depth -exec rm {} \;
+find ~/Desktop -name .DS_Store -depth -exec rm {} \; &>/dev/null
 
 #tmp downloaded files with browsers
-rm -rf ~/Library/Application\ Support/Chromium/Default/File\ System
-rm -rf ~/Library/Application\ Support/Google/Chrome/Default/File\ System
+rm -rf ~/Library/Application\ Support/Chromium/Default/File\ System &>/dev/null
+rm -rf ~/Library/Application\ Support/Google/Chrome/Default/File\ System &>/dev/null
 
 Storage=$(df -h "$HOME" | grep "$HOME" | awk '{print($4)}' | tr 'i' 'B')
 if [ "$Storage" == "0BB" ];
