66fedb9c962c512a209699b9aa1ca36cfcd61ef8
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-12-16 12:16:44 +0100

    fix interpreting shell config file

--- CleanerInstaller.sh
+++ CleanerInstaller.sh
@@ -23,19 +23,19 @@ cp -f ./Cleaner_42.sh "$HOME"
 shell_f=$(echo -n "$SHELL" | awk -F / '{print $3}')
 shell_f="${HOME}/.${shell_f}rc"
 
-if [ "alias cclean='bash ~/Cleaner_42.sh'" == "$(cat ~/"$shell_f" | grep "alias cclean='bash ~/Cleaner_42.sh'")" ]
+if [ "alias cclean='bash ~/Cleaner_42.sh'" == "$(cat "$shell_f" | grep "alias cclean='bash ~/Cleaner_42.sh'")" ]
 then
 	echo -e "\033[33m\n -- cclean Already installed --\n\033[0m"
-	echo -e "\033[36m -- Please, run this command now : [\033[33m source ~/"$shell_f"\033[0m\033[36m ] Then run [\033[33m cclean \033[0m\033[36m]--\n\033[0m" 
+	echo -e "\033[36m -- Please, run this command now : [\033[33m source "$shell_f"\033[0m\033[36m ] Then run [\033[33m cclean \033[0m\033[36m]--\n\033[0m" 
 	exit
 fi
 
-echo "alias cclean='bash ~/Cleaner_42.sh'" >> ~/"$shell_f"
+echo "alias cclean='bash ~/Cleaner_42.sh'" >> "$shell_f"
 
-if [ "alias cclean='bash ~/Cleaner_42.sh'" == "$(cat ~/"$shell_f" | grep "alias cclean='bash ~/Cleaner_42.sh'")" ]
+if [ "alias cclean='bash ~/Cleaner_42.sh'" == "$(cat "$shell_f" | grep "alias cclean='bash ~/Cleaner_42.sh'")" ]
 then
 	echo -e "\033[32m\n -- cclean command has been successfully installed ! Enjoy :) --\n\033[0m"
-	echo -e "\033[36m -- Please, run this command now : [\033[33m source ~/"$shell_f"\033[0m\033[36m ] Then run [\033[33m cclean \033[0m\033[36m]--\n\033[0m" 
+	echo -e "\033[36m -- Please, run this command now : [\033[33m source "$shell_f"\033[0m\033[36m ] Then run [\033[33m cclean \033[0m\033[36m]--\n\033[0m" 
 else
 	echo -e "\033[31m\n -- cclean command has NOT been installed ! :( --\n\033[0m"
 fi
