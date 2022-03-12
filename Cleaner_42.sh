97d87702e47076955b3f473397c423b96221a391
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2022-03-12 15:58:36 +0100

    feat: get access rights on Homebrew caches
    refactor: replace `~` with `HOME` env variable

--- Cleaner_42.sh
+++ Cleaner_42.sh
@@ -3,11 +3,11 @@
 #42login : obouykou
 
 #banner
-echo -e		"\n"
-echo -e		" 		█▀▀ █▀▀ █░░ █▀▀ ▄▀█ █▄░█ "
-echo -e		" 		█▄▄ █▄▄ █▄▄ ██▄ █▀█ █░▀█ "
-echo -en	"\n    	    	   By: "
-echo -e 	"\033[33mOMBHD\033[0m [𝒐𝒃𝒐𝒖𝒚𝒌𝒐𝒖]\n"
+echo -e	"\n"
+echo -e	" 		█▀▀ █▀▀ █░░ █▀▀ ▄▀█ █▄░█ "
+echo -e	" 		█▄▄ █▄▄ █▄▄ ██▄ █▀█ █░▀█ "
+echo -en"\n    	    	   By: "
+echo -e "\033[33mOMBHD\033[0m [𝒐𝒃𝒐𝒖𝒚𝒌𝒐𝒖]\n"
 
 sleep 2
 
@@ -45,40 +45,42 @@ echo -e "\033[33m\n -- Available Storage Before Cleaning : || $Storage || --\033
 echo -e "\033[31m\n -- Cleaning ...\n\033[0m "
 
 #42 Caches
-/bin/rm -rf ~/Library/*.42* &>/dev/null
-/bin/rm -rf ~/*.42* &>/dev/null
-/bin/rm -rf ~/.zcompdump* &>/dev/null
-/bin/rm -rf ~/.cocoapods.42_cache_bak* &>/dev/null
+/bin/rm -rf "$HOME"/Library/*.42* &>/dev/null
+/bin/rm -rf "$HOME"/*.42* &>/dev/null
+/bin/rm -rf "$HOME"/.zcompdump* &>/dev/null
+/bin/rm -rf "$HOME"/.cocoapods.42_cache_bak* &>/dev/null
 
 #Trash
-/bin/rm -rf ~/.Trash/* &>/dev/null
+/bin/rm -rf "$HOME"/.Trash/* &>/dev/null
 
-#General Cache files
-/bin/rm -rf ~/Library/Caches/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Caches/* &>/dev/null
+#General Caches files
+#giving access rights on Homebrew caches, so the script can delete them
+/bin/chmod -R 777 "$HOME"/Library/Caches/Homebrew &>/dev/null
+/bin/rm -rf "$HOME"/Library/Caches/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Caches/* &>/dev/null
 
 #Slack, VSCode, Discord and Chrome Caches
-/bin/rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/discord/Cache/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/discord/Code\ Cache/js* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Service\ Worker/CacheStorage/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Application\ Cache/* &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Code/User/workspaceStorage/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/discord/Cache/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/discord/Code\ Cache/js* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Service\ Worker/CacheStorage/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/Application\ Cache/* &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/* &>/dev/null
 
 #.DS_Store files
-find ~/Desktop -name .DS_Store -depth -exec /bin/rm {} \; &>/dev/null
+find "$HOME"/Desktop -name .DS_Store -depth -exec /bin/rm {} \; &>/dev/null
 
 #tmp downloaded files with browsers
-/bin/rm -rf ~/Library/Application\ Support/Chromium/Default/File\ System &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Chromium/Profile\ [0-9]/File\ System &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Default/File\ System &>/dev/null
-/bin/rm -rf ~/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/File\ System &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Chromium/Default/File\ System &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Chromium/Profile\ [0-9]/File\ System &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/File\ System &>/dev/null
+/bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Profile\ [0-9]/File\ System &>/dev/null
 
 #things related to pool (piscine)
-/bin/rm -rf ~/Desktop/Piscine\ Rules\ *.mp4
-/bin/rm -rf ~/Desktop/PLAY_ME.webloc
+/bin/rm -rf "$HOME"/Desktop/Piscine\ Rules\ *.mp4
+/bin/rm -rf "$HOME"/Desktop/PLAY_ME.webloc
 
 #calculating the new available storage after cleaning
 Storage=$(df -h "$HOME" | grep "$HOME" | awk '{print($4)}' | tr 'i' 'B')
@@ -89,6 +91,6 @@ fi
 sleep 1
 echo -e "\033[32m -- Available Storage After Cleaning : || $Storage || --\n\033[0m"
 
-echo -e		"\n	       report any issues to me in:"
-echo -e		"		   GitHub   ~> \033[4;1;34mombhd\033[0m"
-echo -e		"	   	   42 Slack ~> \033[4;1;34mobouykou\033[0m\n"
+echo -e	"\n	       report any issues to me in:"
+echo -e	"		   GitHub   ~> \033[4;1;34mombhd\033[0m"
+echo -e	"	   	   42 Slack ~> \033[4;1;34mobouykou\033[0m\n"
